040c27eca59e4493de2c0f519e90ca07540d6a58
Kejdi Kishta <155896977+KejdiKishta@users.noreply.github.com>
2024-04-29 15:53:20 +0200

    add: bonus

--- script.js
+++ script.js
@@ -13,11 +13,9 @@ createApp({
             emails: [],
         }
     },
-    created() {
-        this.generateEmails();
-    },
     methods: {
         generateEmails: function () {
+            this.emails = [];
             for (let i = 0; i < 10; i++) {
                 axios
                 .get("https://flynn.boolean.careers/exercises/api/random/mail")
